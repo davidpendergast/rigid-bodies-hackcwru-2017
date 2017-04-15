2da4fd811ccf2b52f22dba3cc7b9ff54773c40d0
dpendergast <dlp75@case.edu>
2017-04-15 14:07:12 -0400

    initial commit

---
+++ src/physics/Edge.java
@@ -0,0 +1,16 @@
+package physics;
+
+import java.awt.Color;
+
+public class Edge {
+    
+    public Vector2d p1, p2;
+    public Color color;
+    
+    public Edge(Vector2d p1, Vector2d p2) {
+        
+    }
+    
+    
+
+}
---
+++ src/physics/Vector2d.java
@@ -0,0 +1,16 @@
+package physics;
+
+import java.awt.Color;
+
+public class Vector2d {
+    
+    public double x;
+    public double y;
+    public Color color;
+    
+    public Vector2d(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
